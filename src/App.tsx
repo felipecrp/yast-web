f38ee7841d3cb0f365ff3fd941a5180400651d52
Felipe Curty <felipecrp@gmail.com>
2022-06-04 00:23:42 -0300

    Create initial layout

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import StatusPage from './status_report/status_page';
+
+function App() {
+  return (
+    <div className="app">
+      <StatusPage></StatusPage>
+    </div>
+  );
+}
+
+export default App;
